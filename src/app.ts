7e62d837e70af798a709af2079ee77d83c8edacc
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-06-11 11:08:05 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import express, { json } from 'express';
+import express, { json } from "express";
+import cors from "cors";
 
-import authRouter from './routes/authRouter';
-import urlRouter from './routes/urlsRouter';
-import userRouter from './routes/usersRouter';
+import authRouter from "./routes/authRouter";
+import urlRouter from "./routes/urlsRouter";
+import userRouter from "./routes/usersRouter";
 
 const app = express();
+app.use(cors());
 app.use(json());
 app.use(authRouter);
 app.use(urlRouter);
